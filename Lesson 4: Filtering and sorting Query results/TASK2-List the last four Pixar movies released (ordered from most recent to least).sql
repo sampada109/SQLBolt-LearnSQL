576b2371a9e63e7ed022cd16a4c787eaf8b9365f
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-02-02 22:05:56 +0530

    Create TASK2-List the last four Pixar movies released (ordered from most recent to least).sql

---
+++ Lesson 4: Filtering and sorting Query results/TASK2-List the last four Pixar movies released (ordered from most recent to least).sql
@@ -0,0 +1,9 @@
+/* Order by - arranging wrt year in 
+descending order - which will give most recent to oldest
+limit - only listing 4 values
+*/
+
+SELECT * 
+FROM movies
+ORDER BY Year DESC
+LIMIT 4;
