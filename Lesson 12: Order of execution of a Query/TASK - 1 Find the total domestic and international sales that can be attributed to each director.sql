db15e657252d89d76596ab2f46e6aa5fcdac93f6
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-02-28 21:57:07 +0530

    Create TASK - 1 Find the total domestic and international sales that can be attributed to each director.sql

---
+++ Lesson 12: Order of execution of a Query/TASK - 1 Find the total domestic and international sales that can be attributed to each director.sql
@@ -0,0 +1,7 @@
+-- Find the total domestic and international sales that can be attributed to each director
+
+SELECT director, SUM(domestic_sales + international_sales) as Cumulative_sales_from_all_movies
+FROM movies
+    INNER JOIN boxoffice
+        ON movies.id = boxoffice.movie_id
+GROUP BY director;
