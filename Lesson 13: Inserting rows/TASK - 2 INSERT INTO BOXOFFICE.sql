82bd1de219d0b280c96cb36655fde60129e9d8e2
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-02-28 22:10:36 +0530

    Create TASK - 2 INSERT INTO BOXOFFICE.sql

---
+++ Lesson 13: Inserting rows/TASK - 2 INSERT INTO BOXOFFICE.sql
@@ -0,0 +1,4 @@
+-- Toy Story 4 has been released to critical acclaim! It had a rating of 8.7, and made 340 million domestically and 270 million internationally. Add the record to the BoxOffice table.
+
+INSERT INTO Boxoffice
+VALUES(15 , 8.7 , 340*100000 , 270*100000);
