7c64125ebe51fc86704a8cac4e0cb60e1923b8c9
sampada109 <143282629+sampada109@users.noreply.github.com>
2024-02-28 22:08:26 +0530

    Create Add the studio's new production, Toy Story 4 to the list of movies (you can use any director).sql

---
+++ Lesson 13: Inserting rows/Add the studio's new production, Toy Story 4 to the list of movies (you can use any director).sql
@@ -0,0 +1,4 @@
+-- Add the studio's new production, Toy Story 4 to the list of movies (you can use any director)
+
+INSERT INTO Movies(Title, Director , Year , Length_minutes)
+VALUES('Toy Story 4', 'John Lasseter', 2023 , 100);
